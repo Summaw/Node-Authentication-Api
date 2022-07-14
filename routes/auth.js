b211c20e9f75b73da90f60e2f2720721a6bfa198
Summer <98126132+UmRange@users.noreply.github.com>
2022-07-14 14:02:00 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,31 +2,32 @@ const express = require('express')
 const router = express.Router()
 const User = require('../modals/user')
 const rounds = 10
+const bcrypt = require('bcrypt')
 const url = require('url')
 const axios = require('axios');
 
 const jwt = require('jsonwebtoken')
-const tokenSecret = "RANDOM-SECRET-HERE"
+const tokenSecret = "random"
 
 const middleware = require('../middleware')
+const { match } = require('assert')
 
 
 router.get('/login', (req, res) => {
     User.findOne({email: req.body.email})
     .then(user => {
-        if(!user) res.status(404).json({error: 'no user with that email was found'})
-        else {
+        if(!user) res.status(404).json({error: 'No user with that email found!'})
+        else{
             bcrypt.compare(req.body.password, user.password, (error, match) => {
                 if (error) res.status(500).json(error)
                 else if (match) res.status(200).json({token: generateToken(user)})
-                else res.status(403).json({error: 'Password does not match'})
+                else res.status(403).json({error: 'password does not match!'})
             })
         }
     })
-    .catch(error => {
+    .catch (error => {
         res.status(500).json(error)
     })
-
 });
 
 router.get('/jwt-test', middleware.verify, (req, res) => {
@@ -34,15 +35,20 @@ router.get('/jwt-test', middleware.verify, (req, res) => {
 })
 
 router.post('/signup', (req, res) => {
+    bcrypt.hash(req.body.password, rounds, (error, hash) =>{
+        if(error) req.status(500).json(error)
+        else{
             const newUser = User({email: req.body.email, password: hash})
             newUser.save()
             .then(user => {
-                res.status(200).json(user)
+                res.status(200).json({token: generateToken(user)})
             })
             .catch(error => {
                 res.status(500).json(error)
+            })
+        }
     })
-});
+})
 
 
 function generateToken(user){
